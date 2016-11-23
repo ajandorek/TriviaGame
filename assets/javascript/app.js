efb42326c817cfea6145f0d299eb5a6989139e3e
Alec Jandorek <alecjandorek@Alecs-MacBook-Pro.local>
2016-11-23 16:34:54 -0600

    Trivia game v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,33 @@
 	var quiz = [{
 		question: "Who won the first Super Bowl?",
 		options: ["The Green Bay Packers", "The Kansas City Chiefs", "The Oakland Raiders", "The New York Giants"],
-		correctAnswer: 0
+		correctAnswer: 0,
+		correctImage: "assets/images/correct1.jpg",
+		incorrectImage: "assets/images/fail1.gif"
 	}, {
 		question: "Who has the most rushing Touchdowns in NFL History?",
 		options: ["Walter Payton", "LaDanian Tomlinson", "Jim Brown", "Emmitt Smith"],
-		correctAnswer: 3
+		correctAnswer: 3,
+		correctImage: "assets/images/correct2.jpg",
+		incorrectImage: "assets/images/fail2.gif"
 	}, {
 		question: "Which player has the most consecutive games with 5 reception + 50 yards receiving?",
 		options: ["Jerry Rice", "Steve Smith Sr.", "Antonio Brown", "Larry Fitzgerald"],
-		correctAnswer: 2
+		correctAnswer: 2,
+		correctImage: "assets/images/correct3.gif",
+		incorrectImage: "assets/images/fail5.gif"
 	}, {
 		question: "Which team has won the most Super Bowls?",
 		options: ["The New England Patriots", "The San Francisco 49ers", "The Pittsburgh Steelers", "The Green Bay Packers"],
-		correctAnswer: 2
+		correctAnswer: 2,
+		correctImage: "assets/images/correct4.jpg",
+		incorrectImage: "assets/images/fail4.gif"
 	}, {
 		question: "Who Dey?",
 		options: ["The New Orleans Saints", "The Cincinnati Bengals", "The Buffalo Bills", "The Oakland Raiders"],
-		correctAnswer: 1
+		correctAnswer: 1,
+		correctImage: "assets/images/correct5.png",
+		incorrectImage: "assets/images/fail3.gif"
 	}];
 
 	var currentQuestion = 0;
@@ -30,9 +40,10 @@
 
 $(document).ready(function(){
 
-	$('#time').html("<h2>Welcome to NFL Trivia!</h2>");
-	$('#question').html("<div class='play'>Play</div>");
+	$('#question').html("<h2>Welcome to NFL Trivia!</h2>");
+	$('#image').html("<button class='button btn-primary play'>Play</div>");
 	$('.play').click(function(){
+	$('#image').empty();
 	showCurrentQuestion();
 	run();
 		$('body').on('click', '.option', function(){
@@ -41,21 +52,24 @@ $(document).ready(function(){
 			clearInterval(counter);
 			$('#time').empty();
 			$('#question').html("<h1>That's Correct!</h1>");
-			$('#option').html("<h2>Great Job!</h2>");
+			$('#image').html("<img src=" + quiz[currentQuestion].correctImage + " />");
 			correct++;
 			currentQuestion++;
 			setTimeout(function(){number = 10}, 5000);
 			$('#options').empty();
+			setTimeout(function(){$('#image').empty()}, 4900);
 		}
 		else if ($(this).text() !== quiz[currentQuestion].options[quiz[currentQuestion].correctAnswer]) {
 			console.log("loss");
 			clearInterval(counter);
 			$('#time').empty();
 			$('#question').html("<h1>That's incorrect! The correct answer is " + quiz[currentQuestion].options[quiz[currentQuestion].correctAnswer] + "!<h1>");
+			$('#image').html("<img src=" + quiz[currentQuestion].incorrectImage + " />");
 			incorrect++
 			currentQuestion++
 			setTimeout(function(){number = 10}, 5000);
 			$('#options').empty();
+			setTimeout(function(){$('#image').empty()}, 4900);
 		}
 		if (currentQuestion >= quiz.length){
 			clearInterval(counter);
@@ -64,8 +78,9 @@ $(document).ready(function(){
 			$('#question').html("<p>Correct: " + correct + "</p>" +
 								"<p>Incorrect: " + incorrect + "</p>" +
 								"<p>Unanswerd: "+ unanswered + "</p>");
-			$('#options').html("<button class='playAgain'>Play Again?</div>");
-			$('.playAgain').click(function(){
+			$('#options').html("<button class='button btn-primary playAgain'>Play Again?</div>");
+			$('button').click(function(){
+				console.log("hello");
 				currentQuestion = 0;
 				unanswered = 0;
 				correct = 0;
@@ -75,11 +90,11 @@ $(document).ready(function(){
 				$('#options').empty();
 				showCurrentQuestion();
 			});
-		}else{
+		}
+		else{
 				setTimeout(function(){showCurrentQuestion()}, 5000);
 				setTimeout(function(){run()}, 5000);
 		}
-		
 		})
 	})
 
@@ -102,16 +117,36 @@ $(document).ready(function(){
 	function decrement() {
 		$('#time').html("<h2>Time Remaining: " + number + "</h2>");
       	number--;
-      	if (number === -1){
+      	if (currentQuestion >= quiz.length){
+			clearInterval(counter);
+			console.log("game over");
+			$('#time').html("<h1>Game Over!</h1>")
+			$('#question').html("<p>Correct: " + correct + "</p>" +
+								"<p>Incorrect: " + incorrect + "</p>" +
+								"<p>Unanswerd: "+ unanswered + "</p>");
+			$('#options').html("<button class='button btn-primary playAgain'>Play Again?</div>");
+			$('.playAgain').click(function(){
+				currentQuestion = 0;
+				unanswered = 0;
+				correct = 0;
+				incorrect = 0;
+				number = 10;
+				run();
+				$('#options').empty();
+				showCurrentQuestion();
+			});
+      	} else if (number === -1){
       		clearInterval(counter);
       		$('#time').empty();
 			$('#question').html("<h1>Time has expired! The correct answer is " + quiz[currentQuestion].options[quiz[currentQuestion].correctAnswer] + "!<h1>");
+			$('#image').html("<img src=" + quiz[currentQuestion].incorrectImage + " />");
 			unanswered++;
 			currentQuestion++;
 			number = 10
 			$('#options').empty();
 			setTimeout(function(){showCurrentQuestion()}, 5000);
 			setTimeout(function(){run()}, 5000);
+			setTimeout(function(){$("#image").empty()}, 4900);
 
 		}
     };
